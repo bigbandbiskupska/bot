e16d18c868e483f8beee01d612d849bfd0101143
Kryštof Tulinger <k.tulinger@seznam.cz>
2016-06-29 23:08:12 +0200

    modifying router

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -7,16 +7,25 @@ use Nette\Application\Routers\RouteList;
 use Nette\Application\Routers\Route;
 
 
-class RouterFactory
+class RouterFactory extends Nette\Object
 {
+
+    public $onCreate = array ();
+    public $onDone = array ();
 
 	/**
 	 * @return Nette\Application\IRouter
 	 */
-	public static function createRouter()
+	public function createRouter()
 	{
 		$router = new RouteList;
+
+		$this->onCreate($router);
+
 		$router[] = new Route('[home]', 'Homepage:default');
+
+		$this->onDone($router);
+
 		//$router[] = new Route('<presenter>/<action>[/<id>]', 'Homepage:default');
 		return $router;
 	}
